cc9b3c1e48bf510d06d06afac8f1435dc5a1127c
GitForSchool1 <fabio.puzzo@itiscuneo.eu>
2024-03-10 11:47:08 +0100

    Fixing security and fixing structure

---
+++ README.md
@@ -0,0 +1,11 @@
+# Prenotazione Colloqui GPOI
+Progetto di GPOI con prenotazione dei colloqui
+To Do:
+
+Finished:
+- [x] Users Stories (Fabio)
+- [x] Schema entità relazione (Manassero)
+- [x] Feature (Simone)
+- [x] Analisi generale (Manassero)
+- [x] Casi d'uso (Kevin)
+- [x] Diagrammi UML (Manassero)
---
+++ tests/TestSicurezza/Login.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include_once "Sicurezza.php";
+        if($_SERVER['REQUEST_METHOD'] == "POST"){
+            if(isset($_POST['btnInvia'])){
+                $username = $_POST['txtUsername'];
+                $password = Sicurezza::creaPassword($_POST['txtPassword']);
+                $mail = Sicurezza::isEmailValid($_POST['txtEmail']);
+                if($mail==1){
+                    $ret = LoginDAO::login($mail,$username); 
+                    /*
+                    Da Implementare
+                    Ritorna 
+                    -> 1 se il login è ok (Username e mail sono corretti)
+                    -> 0 se uno dei due è sbagliato
+                    -> -1 se non esistono -> suggerisce la creazione di un account
+                    
+                    */
+                    switch ($ret) {
+                        case 1:
+                            header("location: stuff.html"); //Da cambiare
+                            break;
+                        case 0:
+                            echo "<h1>Mail o Password errati</h1>";
+                            break;
+                        case -1:
+                            echo "<h1>L'account non esiste</h1>";
+                            break;
+                    }
+                }
+            }
+        }
+
+    ?>
+
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form method="post">
+        <label >Username: </label>
+        <input type="text" name="txtUsername">
+        <br>
+        <label >Password: </label>
+        <input type="password" name="txtPassword">
+        <br>
+        <label >Email: </label>
+        <input type="text" name="txtEmail">
+        <br>
+        <input type="submit" name="btnInvia" id="btnInvia" value="Invia">
+    </form>
+</body>
+</html>

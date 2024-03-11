10945cc374c1a9d29a19975ae416fb5f2cf49303
cAb4co <paolo.mullace@itiscuneo.eu>
2024-03-11 12:37:40 +0100

    Adding models

---
+++ app/Models/Archivio.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Archivio extends Models
+{
+    /***
+     * $this->attributes['id'] - int 
+     * $this->attributes['name'] - string
+     * $this->attributes['cognome'] - string
+     * $this->attributes['documenti'] - string
+     * $this->attributes['tipo'] - string
+     * $this->attributes['email'] - string
+     * $this->attributes['created_at'] - timestamp
+     * $this->attributes['updated_at'] - timestamp
+     */
+
+
+     
+}
---
+++ app/Models/Messaggistica.php
@@ -0,0 +1,12 @@
+<?php
+
+    class Messaggistica{
+        private int $id_sender = 0;
+        private int $id_reciver = 0;
+        private string $message = "";
+
+        
+
+    }
+
+?>

9a0aaa9dcc6e6c56ddf58902d5c2a1d54d4fef00
kevin.tunaru <kevin.tunaru@itiscuneo.eu>
2024-03-11 12:58:19 +0100

    Fixing 500 error

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,8 @@
         <div class="navbar-nav ms-auto">
           <a class="nav-link active" href="{{route('home.index')}}">Home</a>
           <a class="nav-link active" href="{{route('home.about')}}">About</a>
-          <a class="nav-link active" href="{{route('products.index')}}">Products</a>
+          <a class="nav-link active" href="{{route('products.index')}}">Login</a>
+          
         </div>
       </div>
     </div>

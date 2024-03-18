0e3269b95a770e43b2657d0ec685b0818f3f356c
GitForSchool1 <fabio.puzzo@itiscuneo.eu>
2024-03-18 12:46:30 +0100

    Jesus Help

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,8 +3,7 @@
 @section('subtitle','Benvenuto nella pagina per la prenotazione dei colloqui')
 @section('content')
     <div class="row">
-        <a class="nav-link active" href={{route('login')}}>Login</a>
-        <a class="nav-link active" href={{route('register')}}>Registrati</a> <!-- Problema con la registrazione perché i professori potrebbero avere dei nomi in comune con gli alunni... esempio "Federico Dutto" (Alunno) "Federico Dutto" (Professore) -->
+        <label>Pagina Iniziale</label>
     </div>
 
 @endsection

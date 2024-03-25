1cc7404f96db46b66456dfdae14ef90afb5d3f01
GitForSchool1 <fabio.puzzo@itiscuneo.eu>
2024-03-25 13:07:18 +0100

    Adding admin route archivio

--- database/migrations/2024_03_11_105835_create_archivio_table.php
+++ database/migrations/2024_03_11_105835_create_archivio_table.php
@@ -30,7 +30,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('archivi');
+        Schema::dropIfExists('archivios');
     }
 
 
--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,13 @@ Route::get('/','App\Http\Controllers\HomeController@index')->name("home.index");
 
 Route::get('/about','App\Http\Controllers\HomeController@about')->name("home.about");
 
-Route::get('/products.index','App\Http\Controllers\ProductsController@index')->name("products.index");
 
-Route::get('/products/{id}','App\Http\Controllers\ProductsController@show')->name("products.show");
 
 Route::get('/admin','App\Http\Controllers\AdminController@index')->name("admin.home.index");
 
 Route::get('/admin/archivio','App\Http\Controllers\AdminArchivioController@index')->name("admin.home.index");
+Route::get('/admin/archivio','App\Http\Controllers\AdminArchivioController@index')->name("admin.archivio.index");
+
 
 Route::post('/admin/archivio/store','App\Http\Controllers\AdminArchivioController@store')->name('admin.archivio.store');
 

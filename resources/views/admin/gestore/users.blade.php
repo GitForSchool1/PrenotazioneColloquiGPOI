4dac3489c89af50cd37b6ff019b42e297ffa63e1
GitForSchool1 <fabio.puzzo@itiscuneo.eu>
2024-04-16 15:04:28 +0200

    Aggiunto users.blade.php e delle migration

---
+++ resources/views/admin/gestore/users.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.php')
+@section('title',$viewData['title'])
+@section('subtitle',$viewData['subtitle'])
+@section('content')
+
+    <div class="card">
+        <div class="card-body">
+            Show all Users
+        </div>
+    </div>
+
+@endsection

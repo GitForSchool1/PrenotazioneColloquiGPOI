6574f448e0bdd9501fb66d4b698330349e05af06
GitForSchool1 <fabio.puzzo@itiscuneo.eu>
2024-04-28 19:37:34 +0200

    Adding specific routes for "professore" 	modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,20 @@ Route::middleware("admin")->group(function(){
 
     Route::get('/admin','App\Http\Controllers\AdminController@index')->name("admin.home.index");
     Route::get('/admin/archivio','App\Http\Controllers\AdminArchivioController@store')->name('admin.archivio.store');
-    Route::get('/admin/report','App\Http\Controllers\AdminController@show')->name('admin.report.show');
+    Route::get('/admin/report','App\Http\Controllers\AdminReportController@show')->name('admin.report.show');
     Route::delete('/admin/archivio/{id}/delete','App\Http\Controllers\AdminArchivioController@delete')->name('admin.archivio.delete');
     Route::get('/admin/gestore/users','App\Http\Controllers\AdminGestoreController@index')->name('admin.gestore.users');
     Route::get('/admin/gestore/{id}/edit','App\Http\Controllers\AdminGestoreController@edit')->name('admin.gestore.edit');
     Route::put('/admin/gestore/{id}/update','App\Http\Controllers\AdminGestoreController@update')->name('admin.gestore.update');
 });
+
+Route::middleware("professore")->group(function(){
+    /*
+        Upload per i file  -> post
+        Upload degli Orari -> post
+    */
+});
+
 
 //END ADMIN ROUTE
 

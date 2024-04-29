fd73b5780568d88fb23dab02f7e3e6678e547697
GitForSchool1 <fabio.puzzo@itiscuneo.eu>
2024-04-29 12:56:21 +0200

    Delete FUCKING messaggistica

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link rel="shortcut icon" href={{asset('favicon.ico')}}>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous" />
   <link href="{{ asset('/css/app.css') }}" rel="stylesheet" />
   <title>@yield('title', 'Prenotazione')</title>
